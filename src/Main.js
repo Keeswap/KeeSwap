4cc2cb4b689d75dcd32b23116d8b333acd601dea
Keeswap <105143783+Keeswap@users.noreply.github.com>
2022-05-27 19:51:02 +0530

    Update Main.js

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,6 @@
 /* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from "react";
+import React from "react"; 
 
 import logo from "./assets/logokeeswapfinal.png";
 import bgImg from "./assets/mdesign.png";
