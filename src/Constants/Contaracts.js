33dc9fad10fbc063e5104cf7c29732d66d24e8e6
Keeswap <105143783+Keeswap@users.noreply.github.com>
2022-05-27 19:16:43 +0530

    Update Contaracts.js

--- src/Constants/Contaracts.js
+++ src/Constants/Contaracts.js
@@ -1,3 +1,8 @@
-export const busdContract = "0x10ff56C522E3f69F469b53175ACD964B40257598";
-export const keeswap = "0x307c9775E80BE16A99fA10a9043673611D1ebECB";
-export const ico = "0xbFacA1A70DB726330bBA84bf6e0D0E7D2416e76A";
+// export const busdContract = "0x10ff56C522E3f69F469b53175ACD964B40257598";
+export const busdContract = "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56";
+
+// export const keeswap = "0x307c9775E80BE16A99fA10a9043673611D1ebECB";
+export const keeswap = "0x4968f965d8857d68f63272c6e5c88cdc237fb875";
+
+// export const ico = "0xbFacA1A70DB726330bBA84bf6e0D0E7D2416e76A";
+export const ico = "0xdbfee999ed63c1c2f0829f889f6ba44aa64fc83a";
