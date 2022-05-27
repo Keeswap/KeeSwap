d237746e465194b58c4f9fce7009b3c1415286f4
Keeswap <105143783+Keeswap@users.noreply.github.com>
2022-05-27 19:22:09 +0530

    Update web3Connection.js

--- src/Services/web3Connection.js
+++ src/Services/web3Connection.js
@@ -5,7 +5,7 @@ export const provider = new WalletConnectProvider({
   infuraId: "28a21010f56f4c7e8561ab6a8797ebe4",
   bridge: "https://bridge.walletconnect.org",
   rpc: {
-    97: "https://data-seed-prebsc-1-s1.binance.org:8545/",
+    56: "https://data-seed-prebsc-1-s1.binance.org:8545/",
   },
   qrcode: true,
   qrcodeModalOptions: {
