177059db5f865b3f5df2936e56eac27e7c192fc1
Keeswap <105143783+Keeswap@users.noreply.github.com>
2022-06-24 16:37:47 +0530

    Update ICO_Contract.js

--- src/Config/Contract/ICO_Contract.js
+++ src/Config/Contract/ICO_Contract.js
@@ -2,7 +2,7 @@
 export const busdContract = "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56";
 
 // export const keeswap = "0x307c9775E80BE16A99fA10a9043673611D1ebECB";
-export const keeswap = "0x4968f965d8857d68f63272c6e5c88cdc237fb875";
+export const keeswap = "0x221E298573D2ADf5d2Db3F85B0fAdF056e63c2C5";
 
 // export const ico = "0xbFacA1A70DB726330bBA84bf6e0D0E7D2416e76A";
 export const ico = "0x8aa564839f3b87ae495e23de4e6fd755dcb1beda";
